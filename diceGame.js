5bd412079e41115769899aa5c6038357679071e3
unknown <pantazon-b@live.com>
2019-01-15 11:25:13 -0600

    Removed comments from document. Changed all console.log to alert method. Contains working concept of the game.

--- diceGame.js
+++ diceGame.js
@@ -1,78 +1,3 @@
-/*
-	Create a 3-sided dice roll that determines pokemon type
-	Possibly start with an empty array, then use push() method to add each roll to 
-	the array as player moves along. Then make conditionals depending on the value of each array?
-- Assign type to each number
-	- 1 = fire
-	- 2 = water
-	- 3 = grass
-
-Roll 12-sided die to find out if player can continue or game stops
-	1-11 = continue to next turn
-	12 = Team Rocket stole your pokemon, player restarts to first turn
-	
-Create 6-sided dice roll to determine opponent type
-	- 1-2 = fire
-	- 3-4 = water
-	- 5-6 = grass
-
-Create a 4-sided dice roll that determines move for the type you rolled
-If you rolled fire:
-	- 1Scratch "You used Scratch"
-	- 2Growl "You used Growl"
-	- 3Ember "You used Ember"
-	- 4Flamethrower "You used Flamthrower"
-IF you rolled water:
-	- 1Scratch "Used Scratch"
-	- 2Tail Whip "used tail whip"
-	- 3Bubble "used bubble"
-	- 4hydro pump "used hydro pump"
-If you rolled grass:
-	- 1Tackle 
-	- 2Growl
-	- 3Vine Whip
-	- 4Solar Beam
-
-5-sided die
-Determine effectiveness/strength of move used against opponent
-If your type is fire, and your opponent is fire 
-	- 1 Used scratch = Normal damage
-	- 2 used growl = no damage
-	- 3 used ember = minimal damage
-	- 4 used flamethrower = less than normal
-	- 5 attack missed 
-Else if your type is fire and opponent is water
-	- 1 Used scratch = Normal damage
-	- 2 growl = no damage
-	- 3 ember = minimal damage
-	- 4 flamethrower = little damage
-	- 5 attack missed
-Else if your type is fire and opponent is grass
-	- 1 Used scratch = Normal damage
-	- 2 growl = no damage
-	- 3 ember = a lot of damage
-	- 4 flamethrower = max damage
-	- 5 attack missed
-Else if your type is water and opponent is fire
-	- 1 Used scratch = Normal damage
-	- 2 tail whip = no damage
-	- 3 bubble = minimal damage
-	- 4 hydro pump = little damage
-	- 5 attack missed
-etc. for each type/advantages 
-turn chance, who goes first 
-attack types 
-*/
-/*
-Choose a value of the sides array to determine the type of dice being used within the turn
-What step are you on? And which dice is used in this turn?
-
-Array holds types of dice in the game
-Access value of index to determine which dice is being used
-If rolls done = 0, 
-Roll using value of index, randomizing by the value of index + 1
-setInterval ()
-*/
 let sides = [3, 6, 4, 7, 8, 12]; 
 let results = [];
 let rolls = 0; 
@@ -90,89 +15,89 @@ function getResults(){
 	for (let i = 0; i < sides.length; i++) {
 		let rollResult = rollDie(sides[i]);
 		results.push(rollResult);
-		console.log(results);
+		
 	}
 	return;
 }
 
 function turnOne(){
 		if (results[0] === 1){
-		console.log("You received Eevee, the Evolution Pokemon!");
+		alert("You received Eevee, the Evolution Pokemon!");
 		}
 		else if (results[0] === 2){
-		console.log("You received Pikachu, the Electric Mouse Pokemon!");
+		alert("You received Pikachu, the Electric Mouse Pokemon!");
 		}
 		else if (results[0] === 3){
-		console.log("You received Charmander, the Fire Lizard Pokemon!");
+		alert("You received Charmander, the Fire Lizard Pokemon!");
 	}
 	return;
 		}
 
 function turnTwo() {
 	if (results[1] <= 2){
-		console.log("Your opponent sent out Caterpie, a bug type Pokemon!");
+		alert("Your opponent sent out Caterpie, a bug type Pokemon!");
 	}
 	else if (results[1] === 3 || results[1] === 4){
-		console.log("Your opponent sent out Pidgey, a flying type Pokemon!");
+		alert("Your opponent sent out Pidgey, a flying type Pokemon!");
 	}
 	else if (results[1] >= 5){
-		console.log("Your opponent sent out Nidoran, a poison type Pokemon!");
+		alert("Your opponent sent out Nidoran, a poison type Pokemon!");
 	}
 	return;
 }
 
 	
-function turnThree(){
+function turnFive(){
 	switch (results[2]){
-		case 1: console.log("You used Headbutt!");
+		case 1: alert("You used Headbutt!");
 		break;
-		case 2: console.log("You used Tackle!");
+		case 2: alert("You used Tackle!");
 		break;
-		case 3: console.log("You used Dig!");
+		case 3: alert("You used Dig!");
 		break;
-		case 4: console.log("You used Take Down!");
+		case 4: alert("You used Take Down!");
+		break;
+	}
+	return;
+}
+
+function turnThree(){
+	switch (results[3]){
+		case 1: alert("This battle takes place on a Grassland terrain");
+		break;
+		case 2: alert("This battle takes place in Viridian Forest");
+		break;
+		case 3: alert("This battle takes place on a Mountainside terrain");
+		break;
+		case 4: alert("This battle takes place in Mt. Moon!");
+		break;
+		case 5: alert("This battle takes place on the SS Anne cruise ship!");
+		break;
+		case 6: alert("This battle takes place on the piers of Vermillion City");
+		break;
+		case 7: alert("This battle takes place on an all-water terrain");
 		break;
 	}
 	return;
 }
 
 function turnFour(){
-	switch (results[3]){
-		case 1: console.log("This battle takes place on a Grassland terrain");
-		break;
-		case 2: console.log("This battle takes place in Viridian Forest");
-		break;
-		case 3: console.log("This battle takes place on a Mountainside terrain");
-		break;
-		case 4: console.log("This battle takes place in Mt. Moon!");
-		break;
-		case 5: console.log("This battle takes place on the SS Anne cruise ship!");
-		break;
-		case 6: console.log("This battle takes place on the piers of Vermillion City");
-		break;
-		case 7: console.log("This battle takes place on an all-water terrain");
-		break;
-	}
-	return;
-}
-
-function turnFive(){
 	switch (results[4]){
-		case 1: console.log("You are fighting for the Boulder Badge from Pewter City");
+		case 1: alert("You are fighting for the Boulder Badge from Pewter City");
 		break;
-		case 2: console.log("Your are fighting for the Cascade Badge from Cerulean City");
+		case 2: alert("Your are fighting for the Cascade Badge from Cerulean City");
 		break;
-		case 3: console.log("Your are fighting for the Thunder Badge from Vermillion City");
+		case 3: alert("Your are fighting for the Thunder Badge from Vermillion City");
 		break;
-		case 4: console.log("Your are fighting for the Rainbow Badge from Celedon City");
+		case 4: alert("Your are fighting for the Rainbow Badge from Celedon City");
 		break;
-		case 5: console.log("Your are fighting for the Soul Badge from Fuschia City");
+		case 5: alert("Your are fighting for the Soul Badge from Fuschia City");
 		break;
-		case 6: console.log("Your are fighting for the Marsh Badge from Saffron City");
+		case 6: alert("Your are fighting for the Marsh Badge from Saffron City");
 		break;
-		case 7: console.log("Your are fighting for the Volcano Badge from Cinnabar Island");
+		case 7: alert("Your are fighting for the Volcano Badge from Cinnabar Island");
 		break;
-		case 8: console.log("Your are fighting for the Earth Badge from Viridian City");
+		case 8: alert("Your are fighting for the Earth Badge from Viridian City");
 		break;
 	}
 	return;
@@ -180,13 +105,13 @@ function turnFive(){
 
 function turnSix(){
 	if (results[5] <= 4){
-		console.log("You defeated your opponent!");
+		alert("You defeated your opponent!");
 	}
 	else if (5 <= results[5] && results[5] <= 8) {
-		console.log("Your attack missed! Opponent knocked you out. You lose.");
+		alert("Your attack missed! Opponent knocked you out. You lose.");
 	}
 	else if (9 <= results[5]) {
-		console.log("Team Rocket appeared! They stole your Pokemon. You lose.");
+		alert("Team Rocket appeared! They stole your Pokemon. You lose.");
 	}
 	return;
 }
@@ -199,20 +124,3 @@ turnFive();
 turnSix();
 
 
-/*function rollDieFour(){
-	sides = 4;
-	dice4 = Math.floor((Math.random() * sides) + 1);
-	return dice4
-}
-let rollDFour = rollDieFour();
-
-switch(result[i]){
-	case 1:
-	console.log("attack!");
-	case 2:
-	console.log();
-
-
-}
-			
-*/			
